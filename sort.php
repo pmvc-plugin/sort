3a37d7521dcb4d661861e0623014c71693ffdf9b
Hill <hill@kimo.com>
2017-02-16 03:43:58 +0000

    Add unit test

--- sort.php
+++ sort.php
@@ -6,12 +6,25 @@ ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\sort';
 class sort extends \PMVC\PlugIn
 {
     /**
-     * usort($arr, \PMVC\plug('sort')->byColumn('key'));
+     * usort($arr, _byColumn('key'));
      */
-    public function byColumn($key)
+    private function _byColumn($key)
     {
         return function ($a, $b) use ($key) {
             return strnatcmp($a[$key], $b[$key]);
         }; 
+    }
+
+    /**
+     * Need assign with array, due to plugin can't pass by ref
+     * @params array $params[0] The sort array
+     * @params string $params[1] Key which you want sorted
+     * Usage:
+     * $arr = [[0,3], [0,1]];
+     * \PMVC\plug('sort')->byColumn([&$arr, '1']); //sort with key 1
+     */
+    public function byColumn(array $params)
+    {
+        usort($params[0], $this->_byColumn($params[1]));
     }
 }
--- tests/test.php
+++ tests/test.php
@@ -15,4 +15,21 @@ class SortTest extends PHPUnit_Framework_TestCase
         $this->assertContains($this->_plug,$output);
     }
 
+    function testSortByColumn()
+    {
+        $array = [
+            [0,1,2],
+            [0,3,2],
+            [0,2,2],
+        ];
+        
+        $expected = [
+            [0,1,2],
+            [0,2,2],
+            [0,3,2],
+        ];
+        $plug = \PMVC\plug($this->_plug);
+        $plug->byColumn([&$array,1]);
+        $this->assertEquals($expected, $array);
+    }
 }
